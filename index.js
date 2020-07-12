2b40c002bc46897b2adb63f6879d490d4815ba32
anaiel <an.leclabart@gmail.com>
2020-07-12 12:43:37 +0200

    :tada: Create simple interaction

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+require("dotenv").config();
+
 const Discord = require("discord.js");
 const bot = new Discord.Client();
+
+bot.on("ready", () => {
+  console.log("RDBot est connecté.");
+});
+
+bot.on("message", (msg) => {
+  if (msg.content === "Salut") msg.reply("Bonjour");
+});
+
+bot.login(process.env.TOKEN);
