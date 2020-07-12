44b59116aa6508e4a3f8cfb471254d3af7c47db7
anaiel <an.leclabart@gmail.com>
2020-07-12 14:13:11 +0200

    :sparkles: List available roles

--- messageHandler.js
+++ messageHandler.js
@@ -7,7 +7,7 @@ exports.messageHandler = function (msg) {
   const availableCommands = {
     // aide: handleHelp,
     // créer: handleCreate,
-    // liste: handleList,
+    liste: handleList,
     // role: handleRole,
   };
 
@@ -18,4 +18,39 @@ exports.messageHandler = function (msg) {
   availableCommands[command[0]](msg, command.slice(1));
 };
 
-function handleList(msg, command) {}
+function handleList(msg, command) {
+  const availableCategories = {
+    1752220: "ligues",
+  };
+  let reply = "";
+
+  if (!command[0] || !availableCategories[command[0]]) {
+    if (command[0])
+      reply +=
+        "La catégorie demandée n'existe pas. Voici tous les rôles disponibles : \n";
+    const rolesByCategory = getAllRoles(
+      msg.guild.roles.cache,
+      availableCategories
+    );
+    Object.keys(rolesByCategory).forEach((category) => {
+      reply += `**${category}** : ${rolesByCategory[category].join(", ")}\n`;
+    });
+  } else {
+    const roles = msg.guild.roles.filter(
+      (role) => availableCategories[role.color] === command[0]
+    );
+    reply += `**${command[0]}** : ${roles.join(", ")}\n`;
+  }
+
+  msg.reply(reply);
+}
+
+function getAllRoles(roles, availableCategories) {
+  const rolesByCategory = {};
+  roles.forEach((role) => {
+    const category = availableCategories[role.color] || "autre";
+    if (!rolesByCategory[category]) rolesByCategory[category] = [role.name];
+    else rolesByCategory[category].push(role.name);
+  });
+  return rolesByCategory;
+}
