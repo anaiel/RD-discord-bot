58ff2f4e8ea4705b3e26237c5c7bb9545616a3a7
anaiel <an.leclabart@gmail.com>
2020-07-13 13:46:48 +0200

    :pencil: Document categories

--- RoleCategory.js
+++ RoleCategory.js
@@ -1,3 +1,15 @@
+/**
+ * Roles are sorted by categories, which are artificially differentiated on
+ * Discord by color.
+ * New categories can be added by adding an item to the _data array:
+ *   - name should be unique
+ *   - color should be unique
+ *   - mentionable should be a boolean corresponding to whether or not the
+ *     roles in the category should be taggable by everyone
+ *   - permissions should be an integer calculated with
+ *     https://discordapi.com/permissions.html (use the standard 1144114257 if
+ *     not creating a role with special privileges)
+ */
 exports.RoleCategories = {
   _data: Object.freeze([
     Object.freeze({
